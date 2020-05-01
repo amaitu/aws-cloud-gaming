233f0a9a18dcfc89539777a24a3059001882a522
amaitu <steve@sgbarker.com>
2020-05-01 20:36:53 +0100

    Modify start script for my own use.

--- start.sh
+++ start.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-# Make sure we're on my personal AWS account
-export AWS_PROFILE=personal
 export TF_IN_AUTOMATION=TRUE
 
 # Terraform Apply
 terraform apply -auto-approve > /dev/null
 
 instance="${terraform output spot_instance_id}"
-
-echo "Instance ${instance} is provisioning."
 
 export TF_IN_AUTOMATION=FALSE
